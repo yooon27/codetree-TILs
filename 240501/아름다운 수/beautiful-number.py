5b1e4f26c550bc8ca236c0c5407ff943eb19c7c2
yooon27 <124117305+yooon27@users.noreply.github.com>
2024-05-01 21:42:45 +0900

    Title: 아름다운 수, Time: 436ms, Memory: 28MB, Status: Passed - CodeTree

--- 240501/아름다운 수/beautiful-number.py
+++ 240501/아름다운 수/beautiful-number.py
@@ -1,35 +1,42 @@
-import sys
-input = sys.stdin.readline
-
+# 변수 선언 및 입력:
 n = int(input())
 ans = 0
 seq = list()
 
+
 def is_beautiful():
+    # 연달아 같은 숫자가 나오는 시작 위치를 잡습니다.
     i = 0
     while i < n:
-        if + seq[i] - 1 >= n:
+        # 만약 연속하여 해당 숫자만큼 나올 수 없다면
+        # 아름다운 수가 아닙니다.
+        if i + seq[i] - 1 >= n:
             return False
-        
+        # 연속하여 해당 숫자만큼 같은 숫자가 있는지 확인합니다.
+        # 하나라도 다른 숫자가 있다면
+        # 아름다운 수가 아닙니다.
         for j in range(i, i + seq[i]):
             if seq[j] != seq[i]:
                 return False
-        
+            
         i += seq[i]
+        
     return True
+
 
 def count_beautiful_seq(cnt):
     global ans
-
+    
     if cnt == n:
         if is_beautiful():
             ans += 1
         return
-    
-    for i in range(1,5):
+	
+    for i in range(1, 5):
         seq.append(i)
-        count_beautiful_seq(cnt+1)
+        count_beautiful_seq(cnt + 1)
         seq.pop()
+
 
 count_beautiful_seq(0)
 print(ans)
