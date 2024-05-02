dee59bd8d98ab98dab44396c9376d4ead74ed0c3
yooon27 <124117305+yooon27@users.noreply.github.com>
2024-05-02 15:58:17 +0900

    Title: 계단 오르기, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240502/계단 오르기/climbing-stairs.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+
+ans = 0
+arr = [0]*1001
+
+
+arr[2] = 1
+arr[3] = 1
+
+for i in range(4, n+1):
+    arr[i] = arr[i-2] + arr[i-3]
+
+print(arr[n])
