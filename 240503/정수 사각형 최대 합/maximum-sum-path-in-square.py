11c02dcd28c2d5aaa05a4edb119fab486f4f409f
yooon27 <124117305+yooon27@users.noreply.github.com>
2024-05-03 14:25:05 +0900

    Title: 정수 사각형 최대 합, Time: 133ms, Memory: 25MB, Status: Passed - CodeTree

--- 240503/정수 사각형 최대 합/maximum-sum-path-in-square.py
+++ 240503/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -12,16 +12,16 @@ dp[0][0] = graph[0][0]
 
 # first initialization
 if n > 1:
-    for i in range(n):
-        dp[0][i] += graph[0][i]
-    for j in range(n):
-        dp[j][n-1] += graph[j][n-1]
+    for i in range(1,n):
+        dp[0][i] += graph[0][i] + dp[0][i-1]
+    for j in range(1,n):
+        dp[j][n-1] += graph[j][n-1] + dp[j-1][n-1]
 
     #second initialization(right and down)
-    for i in range(n):
-        dp[i][0] += graph[i][0]
-    for j in range(n):
-        dp[n-1][j] += graph[n-1][j]
+    for i in range(1,n):
+        dp[i][0] += graph[i][0] + dp[i-1][0]
+    for j in range(1,n):
+        dp[n-1][j] += graph[n-1][j] + dp[n-1][j-1]
 
     for i in range(1, n):
         for j in range(1, n):
