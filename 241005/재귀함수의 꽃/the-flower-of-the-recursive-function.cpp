8d10705156fbab01963f7ad14c17778175de4aa1
yooon27 <124117305+yooon27@users.noreply.github.com>
2024-10-05 20:47:58 +0900

    Title: 재귀함수의 꽃, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241005/재귀함수의 꽃/the-flower-of-the-recursive-function.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    for(int i = n; i > 0 ; i--) cout << i << ' ';
+    for(int j = 1; j <= n ; j++) cout << j << ' ';
+    return 0;
+}
