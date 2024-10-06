4ca87d82ecff2233d15abc9943d67f2279eb1a9f
yooon27 <124117305+yooon27@users.noreply.github.com>
2024-10-06 13:37:23 +0900

    Title: 두 선분, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241006/두 선분/two-lines.cpp
+++ 241006/두 선분/two-lines.cpp
@@ -7,8 +7,8 @@ int main() {
     cin >> x1 >> x2 >> x3 >> x4;
     int arr[102];
     for(int i = 0 ; i<101 ; i++) arr[i] = 0;
-    for(int i = x1 ; i<x2 ; i++) arr[i] += 1;
-    for(int i = x3 ; i <x4; i++) arr[i] += 1;
+    for(int i = x1 ; i<=x2 ; i++) arr[i] += 1;
+    for(int i = x3 ; i <=x4; i++) arr[i] += 1;
     for(int i = 0; i < 101; i ++ ){
         if(arr[i] > 1){
             cout << "intersecting";
