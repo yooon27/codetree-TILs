7d177d8c5a4adb1de884453e1ab781065442a7fe
yooon27 <124117305+yooon27@users.noreply.github.com>
2024-10-06 13:35:08 +0900

    Title: 두 선분, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241006/두 선분/two-lines.cpp
+++ 241006/두 선분/two-lines.cpp
@@ -5,16 +5,18 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int x1 , x2, x3, x4;
     cin >> x1 >> x2 >> x3 >> x4;
-    if(x3 <= x2){
-        cout <<"intersecting";
-        exit(0);
-    }
-    else{
-    if(x1 <= x4){
-        cout <<"intersecting";
-        exit(0);
-    }
+    int arr[102];
+    for(int i = 0 ; i<101 ; i++) arr[i] = 0;
+    for(int i = x1 ; i<x2 ; i++) arr[i] += 1;
+    for(int i = x3 ; i <x4; i++) arr[i] += 1;
+    for(int i = 0; i < 101; i ++ ){
+        if(arr[i] > 1){
+            cout << "intersecting";
+            exit(0);
+        }
+
     }
     cout << "nonintersecting";
+    
     return 0;
 }
